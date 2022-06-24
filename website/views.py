140205cf48a79e6696c69d476e6e7525a1fb6831
Winnie Yap Xiang Loo <winnieyap20@gmail.com>
2022-06-25 02:10:54 +0800

    editing

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from unicodedata import category
-from flask import Blueprint, jsonify,request, render_template, flash
+from flask import Blueprint, jsonify,request, render_template, flash, redirect, url_for
 from graphviz import render
 import psycopg2 #pip install psycopg2 
 import psycopg2.extras
@@ -25,12 +25,37 @@ def search():
             flash('Search term is too short', category='error')
         else:
             cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-            s = "SELECT * FROM tokens WHERE token = '" + search_term.lower() + "'"
+            s = "SELECT *, CASE WHEN langID='1' THEN 'English' WHEN langID='2' THEN 'Malay'  WHEN langID='3' THEN 'Chinese' WHEN langID='4' THEN 'Others' END FROM tokens WHERE token = '" + search_term.lower() + "'"
             cur.execute(s) # Execute the SQL
             token = cur.fetchall()
             flash('Searched!', category='success')
 
     return render_template('search.html', token=token)
+
+
+# work on it for filter view 
+@views.route('/radio_filter', methods=['GET','POST'])
+def radio_filter():
+    user_answer = request.form.get('search')
+    return redirect(url_for('views.Index'))
+
+
+# @views.route('/search',methods=['GET', 'POST'])
+# def search():
+#     token=""
+#     if request.method == 'POST':
+#         search_term = request.form.get('search')
+
+#         if len(search_term)<1:
+#             flash('Search term is too short', category='error')
+#         else:
+#             cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+#             s = "SELECT * FROM tokens WHERE token = '" + search_term.lower() + "'"
+#             cur.execute(s) # Execute the SQL
+#             token = cur.fetchall()
+#             flash('Searched!', category='success')
+
+#     return render_template('search.html', token=token)
 
 
 
